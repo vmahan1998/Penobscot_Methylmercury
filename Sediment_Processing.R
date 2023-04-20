0bea39925ea474f2cccd13ca472cf2ec2b8a93eb
Mahan <1149184763121005@mil>
2023-04-20 15:43:59 -0400

    Updates

---
+++ Sediment_Processing.R
@@ -0,0 +1,10 @@
+# ALewife Spawning Behavior Suitability Indices
+
+# Alewife Juvenile Suitability Indices
+alewife.juv.depth <- c(0,1,10,10) #meters
+alewife.juv.depth.SIV <- c(0,1,1,0) #suitability
+alewife.juv.sal <- c(0,10,25,30) #psu
+alewife.juv.sal.SIV <- c(0,1,1,0) #suitability
+alewife.juv.temp <-c(6,12,22,30) #celcius
+alewife.juv.temp.SIV <- c(0,1,1,0) #suitability
+alewife.juvenile <-cbind(alewife.juv.depth,alewife.juv.depth.SIV,alewife.juv.sal,alewife.juv.sal.SIV,alewife.juv.temp,alewife.juv.temp.SIV)
